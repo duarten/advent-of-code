bb97fd393c68841f9912a63a77974f3e2754395d
Duarte Nunes <duarte@hey.com>
2020-12-05 13:28:03 -0300

    use slices to parse seat id
    Signed-off-by: Duarte Nunes <duarte@hey.com>

--- src/day05.rs
+++ src/day05.rs
@@ -2,26 +2,21 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::ops::Add;
 
-fn parse_id(guide: String) -> Option<usize> {
-    let mut row = 0;
-    let mut col = 0;
-    for g in guide.bytes() {
-        match g {
-            b'F' => row <<= 1,
-            b'B' => row = (row << 1) + 1,
-            b'L' => col <<= 1,
-            b'R' => col = (col << 1) + 1,
-            _ => return None,
-        }
-    }
-    Some(row * 8 + col)
+fn parse_id(guide: &str) -> usize {
+    let row = guide[..7]
+        .chars()
+        .fold(0, |acc, g| (acc << 1) + (g == 'B') as usize);
+    let col = guide[7..]
+        .chars()
+        .fold(0, |acc, g| (acc << 1) + (g == 'R') as usize);
+    row * 8 + col
 }
 
 fn main() {
     let file = File::open("inputs/day05.input").unwrap();
     let ids = io::BufReader::new(file)
         .lines()
-        .filter_map(|l| parse_id(l.unwrap()));
+        .map(|l| parse_id(&l.unwrap()));
     let (highest, smallest, cnt) = ids.fold((0, usize::MAX, 0), |(max, min, sum), x| {
         (usize::max(max, x), usize::min(min, x), sum + x)
     });
