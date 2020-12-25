f0cf2a09eebeaba427a69631dc65f91f023dafd8
Duarte Nunes <duarte@hey.com>
2020-12-25 12:37:53 -0300

    add solution for day 23
    Signed-off-by: Duarte Nunes <duarte@hey.com>

---
+++ src/day23.rs
@@ -0,0 +1,70 @@
+#![feature(iterator_fold_self)]
+
+mod list;
+
+use std::fs;
+use std::num::NonZeroUsize;
+
+use list::{Arena, CircularNodeIterator, NodeId};
+
+struct Cups {
+    arena: Arena<usize>,
+    current: NodeId,
+}
+
+impl Cups {
+    fn new(labels: Vec<usize>) -> Self {
+        let mut arena = Arena::new();
+        (1..=labels.len()).for_each(|x| {
+            arena.new_node(x);
+        });
+        let current = NodeId(NonZeroUsize::new(labels[0]).unwrap());
+        labels
+            .into_iter()
+            .map(|l| NodeId(NonZeroUsize::new(l).unwrap()))
+            .fold_first(|a, b| a.append_after(b, &mut arena));
+        Self { arena, current }
+    }
+
+    fn do_move(&mut self) {
+        let excluded = self.current.pop_after(3, &mut self.arena).unwrap();
+        let target = (1..)
+            .map(|t| ((self.current.0.get() - 1) + self.arena.len() - t) % self.arena.len() + 1)
+            .skip_while(|t| excluded.contains(t, &self.arena))
+            .take(1)
+            .last()
+            .unwrap();
+        NodeId(NonZeroUsize::new(target).unwrap()).append_after(excluded, &mut self.arena);
+        self.current = self.arena[self.current].next
+    }
+
+    fn result(&self, limit: usize) -> Vec<usize> {
+        CircularNodeIterator::new(&self.arena, NodeId(NonZeroUsize::new(1).unwrap()))
+            .skip(1)
+            .take(limit)
+            .map(|n| n.data)
+            .collect()
+    }
+}
+
+fn main() {
+    let mut labels: Vec<_> = fs::read_to_string("inputs/day23.input")
+        .unwrap()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect();
+    let mut c = Cups::new(labels.clone());
+    (0..100).for_each(|_| c.do_move());
+    let r = c
+        .result(8)
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join("");
+    println!("part 1: {}", r);
+    labels.extend(10..=1000000);
+    let mut c = Cups::new(labels);
+    (0..10000000).for_each(|_| c.do_move());
+    let r = c.result(2).iter().product::<usize>();
+    println!("part 2: {}", r);
+}
---
+++ src/list.rs
@@ -0,0 +1,120 @@
+use std::num::NonZeroUsize;
+use std::ops::Index;
+use std::ops::IndexMut;
+
+// Sadness: Rust doesn't have a good linked list implementation
+// with O(1) removals.
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct NodeId(pub NonZeroUsize);
+
+impl NodeId {
+    fn index0(self) -> usize {
+        self.0.get() - 1
+    }
+
+    pub fn append_after<T>(self, other: NodeId, arena: &mut Arena<T>) -> NodeId {
+        let id = CircularNodeIterator::new(arena, other).last().unwrap().id;
+        arena[id].next = std::mem::replace(&mut arena[self].next, other);
+        return id;
+    }
+
+    pub fn pop_after<T>(self, cnt: usize, arena: &mut Arena<T>) -> Option<NodeId> {
+        let (last_id, last_next) = if let Some(last) = CircularNodeIterator::new(arena, self)
+            .skip(1)
+            .take(cnt)
+            .last()
+        {
+            (last.id, last.next)
+        } else {
+            return None;
+        };
+        let ret = std::mem::replace(&mut arena[self].next, last_next);
+        arena[last_id].next = ret;
+        Some(ret)
+    }
+
+    pub fn contains<T: PartialEq>(self, val: &T, arena: &Arena<T>) -> bool {
+        CircularNodeIterator::new(arena, self)
+            .find(|n| n.data == *val)
+            .is_some()
+    }
+}
+
+#[derive(Debug)]
+pub struct Node<T> {
+    pub data: T,
+    pub id: NodeId,
+    pub next: NodeId,
+}
+
+pub struct Arena<T> {
+    nodes: Vec<Node<T>>,
+}
+
+impl<T> Arena<T> {
+    pub fn new() -> Arena<T> {
+        Self { nodes: Vec::new() }
+    }
+
+    pub fn len(&self) -> usize {
+        self.nodes.len()
+    }
+
+    pub fn new_node(&mut self, data: T) -> NodeId {
+        let id =
+            NodeId(NonZeroUsize::new(self.nodes.len().wrapping_add(1)).expect("too many nodes"));
+        self.nodes.push(Node { data, id, next: id });
+        id
+    }
+}
+
+impl<T> Index<NodeId> for Arena<T> {
+    type Output = Node<T>;
+
+    fn index(&self, node: NodeId) -> &Node<T> {
+        &self.nodes[node.index0()]
+    }
+}
+
+impl<T> IndexMut<NodeId> for Arena<T> {
+    fn index_mut(&mut self, node: NodeId) -> &mut Node<T> {
+        &mut self.nodes[node.index0()]
+    }
+}
+
+pub struct CircularNodeIterator<'a, T> {
+    arena: &'a Arena<T>,
+    start_at: NodeId,
+    current: Option<NodeId>,
+}
+
+impl<'a, T> CircularNodeIterator<'a, T> {
+    pub fn new(arena: &'a Arena<T>, start_at: NodeId) -> Self {
+        CircularNodeIterator {
+            arena,
+            current: Some(start_at),
+            start_at,
+        }
+    }
+}
+
+impl<'a, T> Iterator for CircularNodeIterator<'a, T> {
+    type Item = &'a Node<T>;
+
+    fn next(&mut self) -> Option<&'a Node<T>> {
+        let current = self.current;
+        match current {
+            Some(id) => {
+                let result = &self.arena[id];
+                self.current = if result.next != self.start_at {
+                    Some(result.next)
+                } else {
+                    None
+                };
+                Some(result)
+            }
+            None => None,
+        }
+    }
+}
