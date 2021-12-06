f85fbf3cc4256eea55b07db4a4d9d1623ee217d4
Duarte Nunes <duarte.nunes@datadoghq.com>
2021-12-06 02:37:39 -0300

    [2021] Add solution for day 6

--- aoc2021/src/day06.rs
+++ aoc2021/src/day06.rs
@@ -1,1 +1,35 @@
-fn main() {}
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+fn run_simulation(mut fishes: [usize; 9], limit: usize) -> usize {
+    for _ in 0..limit {
+        for (pos, cnt) in std::mem::take(&mut fishes).into_iter().enumerate() {
+            if pos == 0 {
+                fishes[6] += cnt;
+                fishes[8] += cnt;
+            } else {
+                fishes[pos - 1] += cnt;
+            }
+        }
+    }
+    fishes.into_iter().sum()
+}
+
+fn main() {
+    let file = File::open("aoc2021/inputs/day06.input").unwrap();
+    let mut fishes = [0; 9];
+    for f in io::BufReader::new(file)
+        .lines()
+        .last()
+        .unwrap()
+        .unwrap()
+        .split(',')
+        .map(|f| f.parse::<usize>().unwrap())
+    {
+        fishes[f] += 1;
+    }
+    println!("part 1: {}", run_simulation(fishes, 80));
+    println!("part 2: {}", run_simulation(fishes, 256));
+}
